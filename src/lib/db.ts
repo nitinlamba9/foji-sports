d2516b3136b5fa8f631f48aec56f3a3fae3c9ff5
nitinlamba9 <nitinlamba0000.com>
2026-01-30 13:34:30 +0530

    Debug: force DB env check marker

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error('MONGODB_URI is not defined');
+  throw new Error('DB_CHECK_VERCEL_2026');
+
 }
 
 /**
